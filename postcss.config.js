ce3eacc680fa10d3c4a1379735f1b1e90b194bbb
UpTheAnte-Brian <brian@uptheante.org>
2025-03-06 14:49:05 -0600

    trying to fix last deploy

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
-export default  {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import tailwindcssConfig from './tailwind.config';
+
+const config = {
+  plugins: [
+    tailwindcss(tailwindcssConfig),
+    autoprefixer,
+  ],
 };
+
+export default config;
