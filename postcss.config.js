0bb6a6c3f52c349880e1998629dcb5bc0d93c515
UpTheAnte-Brian <brian@uptheante.org>
2025-03-06 14:44:59 -0600

    trying to fix last deploy

--- postcss.config.js
+++ postcss.config.js
@@ -1,37 +1,6 @@
-// import autoprefixer from 'autoprefixer';
-// import tailwindcss from 'tailwindcss';
-// import tailwindcssConfig from './tailwind.config';
-
-// const config = {
-//   plugins: [
-//     tailwindcss(tailwindcssConfig),
-//     autoprefixer,
-//   ],
-// };
-
-// export default config;
-
-export const plugins = {
-  tailwindcss: {},
-  autoprefixer: {},
+export default  {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-// import autoprefixer from 'autoprefixer';
-// import tailwindcss from 'tailwindcss';
-
-// import tailwindcssConfig from './tailwind.config';
-
-// export default { plugins: [autoprefixer, tailwindcss(tailwindcssConfig)] };
-
-// // postcss.config.ts
-// import type { Config } from 'postcss';
-
-// const config: (ctx: { env: string }) => Config = (ctx) => ({
-//   plugins: [
-//     require('autoprefixer'),
-//     // Conditionally include plugins based on the environment
-//     ...(ctx.env === 'production' ? [require('cssnano')] : []),
-//   ],
-// });
-
-// export default config;
