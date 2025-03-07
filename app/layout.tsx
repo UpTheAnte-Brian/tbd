b06a782df845672d954483d78c54bc4e16123d9c
UpTheAnte-Brian <brian@uptheante.org>
2025-03-06 22:19:56 -0600

    fixing the tailwind thing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import "./ui/global.css";
+import "@/app/ui/global.css";
 import { inter } from "./ui/fonts";
 import NavBar from "./ui/dashboard/nav-bar";
 import Footer from "./ui/dashboard/footer";
-// import { StyledEngineProvider } from "@mui/material";
+import { StyledEngineProvider } from "@mui/material";
 
 // This sets the title on your browser tab.
 export const metadata = {
@@ -22,18 +22,18 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased flex flex-col min-h-100vh p-1`}
       >
-        {/* <StyledEngineProvider injectFirst> */}
-        <div className="min-h-10svh">
-          <NavBar />
-        </div>
-        <main className="min-h-80svh p-1">
-          {children}
-          {/* <div className="flex-grow outline rounded-lg inset-0"></div> */}
-        </main>
-        <div className="outline rounded-lg inset-0 min-h-10svh">
-          <Footer />
-        </div>
-        {/* </StyledEngineProvider> */}
+        <StyledEngineProvider injectFirst>
+          <div className="min-h-10svh">
+            <NavBar />
+          </div>
+          <main className="min-h-80svh p-1">
+            {children}
+            {/* <div className="flex-grow outline rounded-lg inset-0"></div> */}
+          </main>
+          <div className="outline rounded-lg inset-0 min-h-10svh">
+            <Footer />
+          </div>
+        </StyledEngineProvider>
       </body>
       {/* <body class="flex flex-col min-h-screen">
     <header>...</header>
