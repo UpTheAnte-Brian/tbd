887a5672ae0b2edae9b995c2e368c20ac1e0e8b9
UpTheAnte-Brian <brian@uptheante.org>
2025-04-15 10:13:54 -0500

    trying to fix tailwind issue in vercel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,29 +8,29 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    // extend: {
-    //   fontFamily: {
-    //     satoshi: ["Satoshi", "sans-serif"],
-    //     inter: ["Inter", "sans-serif"],
-    //   },
-    //   gridTemplateColumns: {
-    //     "13": "repeat(13, minmax(0, 1fr))",
-    //   },
-    //   colors: {
-    //     blue: {
-    //       400: "#2589FE",
-    //       500: "#0070F3",
-    //       600: "#2F6FEB",
-    //     },
-    //   },
-    // },
-    // keyframes: {
-    //   shimmer: {
-    //     "100%": {
-    //       transform: "translateX(100%)",
-    //     },
-    //   },
-    // },
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "13": "repeat(13, minmax(0, 1fr))",
+      },
+      colors: {
+        blue: {
+          400: "#2589FE",
+          500: "#0070F3",
+          600: "#2F6FEB",
+        },
+      },
+    },
+    keyframes: {
+      shimmer: {
+        "100%": {
+          transform: "translateX(100%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
