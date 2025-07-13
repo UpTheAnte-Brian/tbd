5578cd137d22b10ab2d3e0a021c88e96812538eb
UpTheAnte-Brian <brian@uptheante.org>
2025-07-13 14:13:19 -0500

    adding account page with profile pic

--- app/page.tsx
+++ app/page.tsx
@@ -18,18 +18,7 @@ export default function Page() {
           <p className={`text-xl text-gray-100 md:text-3xl md:leading-normal`}>
             <strong>Site in Development</strong> <br />
             This is a prototype.
-            {/* <a href="https://uptheante.org/" className="text-blue-500">
-              UpTheAnte.org
-            </a>
-            , brought to you by Ante Up Nation. */}
           </p>
-          {/* <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link> */}
-          {/* <SignIn /> */}
         </div>
         <div className="flex items-center justify-center p-1 md:w-3/5 md:p-3 rounded-lg">
           <MapComponent />
